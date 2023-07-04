82d1461620303f065782920a80c30d2de8bbe4c2
JP Monteagudo <100309883+jpmonteagudo28@users.noreply.github.com>
2023-07-04 00:28:16 -0400

    Added new ReadMe File for scripts. Added new script with factor counting function. Added tidy data containing weak probable prime bases.

--- Script/table_scraper.R
+++ Script/table_scraper.R
@@ -2,7 +2,8 @@ using("rvest", "data.table")
 url1 <- ("https://t5k.org/glossary/page.php?sort=PRP")
 file <- read_html(url1)
 carmichaelTable <- html_table(file); prp_num <- as.data.table(carmichaelTable)
+prp_num[,-1]
 rm("url1","file","carmichaelTable")
 colnames(prp_num) <-c("Base","Composite PRP < 500","Percentage")
 gsub("\r\n","",prp_num$`Composite PRP < 500`);gsub("%","",prp_num$Percentage)
-
+write.table(prp_num, file = "prp_number.txt",sep = "|")
